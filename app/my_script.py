5ad5aaa0763801475064bc0cb0a54849de88e668
hiepnguyen034 <hiepnguyen034@gmail.com>
2019-04-08 17:54:04 -0400

    fail

--- app/my_script.py
+++ app/my_script.py
@@ -1,5 +1,5 @@
 def enlarge(i):
-    return i * 100
+    return i * 100000
 
 if __name__ == "__main__": # "if this script is run from the command-line, then ..."
     original_number = int(input("Please select a number to be enlarged (e.g. 400): "))
