47cea5528d60248f216abcb5c8a09fd02676aad8
hiepnguyen034 <hiepnguyen034@gmail.com>
2019-04-08 17:56:11 -0400

    fix fail

--- test/my_script_test.py
+++ test/my_script_test.py
@@ -2,4 +2,4 @@ from app.my_script import enlarge # <-- NOTE this modification (app.my_script)
 
 def test_enlarge():
     result = enlarge(3)
-    assert result == 300
+    assert result == 3*100000
